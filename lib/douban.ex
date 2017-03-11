d9a58233da49645c422386ff0e0cd3d938a52b13
lyhopq <lyhopq@163.com>
2017-03-11 21:41:33 +0800

    refactoring with '|>'

--- lib/douban.ex
+++ lib/douban.ex
@@ -1,13 +1,14 @@
 defmodule Douban do
   use HTTPotion.Base
 
-  def get_book(book_id) do
-    url = process_url(book_id)
-    IO.puts "Getting data from #{url}"
-    response = HTTPotion.get(url, [:timeout, 15000])
-    
-    {json} = :jiffy.decode response.body
-    Enum.reduce(json, %{}, fn(x, acc) -> 
+  def get_book_by_id(book_id) do
+    book_id
+    |> process_url
+    |> HTTPotion.get([:timeout, 15000])
+    |> Map.fetch!(:body)
+    |> :jiffy.decode
+    |> unpack
+    |> Enum.reduce(%{}, fn(x, acc) ->
       {k, v} = x;
       Map.put(acc, String.to_atom(k), v) end)
   end
@@ -19,5 +20,10 @@ defmodule Douban do
       book_id
     end
     "https://api.douban.com/v2/book/" <> book_id
+  end
+
+  defp unpack(pack) do
+    {vaule} = pack
+    vaule
   end
 end
