e4ac5f43e6b679c23829a9b10f3778572a851100
lyhopq <lyhopq@163.com>
2017-03-11 21:11:45 +0800

    refactoring with reduce

--- lib/douban.ex
+++ lib/douban.ex
@@ -1,73 +1,23 @@
 defmodule Douban do
   use HTTPotion.Base
 
-  def process_url(book_id) do
-    if is_integer(book_id) do
-      # FIXME: complie waring
-      book_id = Integer.to_string(book_id)
-    end
-    "https://api.douban.com/v2/book/" <> book_id
-  end
-  
   def get_book(book_id) do
     url = process_url(book_id)
     IO.puts "Getting data from #{url}"
     response = HTTPotion.get(url, [:timeout, 15000])
+    
+    {json} = :jiffy.decode response.body
+    Enum.reduce(json, %{}, fn(x, acc) -> 
+      {k, v} = x;
+      Map.put(acc, String.to_atom(k), v) end)
+  end
 
-    json = :jiffy.decode response.body
-    {[
-      {"rating", rating},
-      {"subtitle", subtitle},
-      {"author", author},
-      {"pubdate", pubdate},
-      {"tags", tags},
-      {"origin_title", origin_title},
-      {"image", image},
-      {"binding", binding},
-      {"translator", translator},
-      {"catalog", catalog},
-      {"ebook_url", ebook_url},
-      {"pages", pages},
-      {"images", images},
-      {"alt", alt},
-      {"id", id},
-      {"publisher", publisher},
-      {"isbn10", isbn10},
-      {"isbn13", isbn13},
-      {"title", title},
-      {"url", url},
-      {"alt_title", alt_title},
-      {"author_intro", author_intro},
-      {"summary", summary},
-      {"ebook_price", ebook_price},
-      {"series", series},
-      {"price", price}
-    ]} = json
-    %{
-      rating: rating,
-      subtitle: subtitle,
-      author: author,
-      pubdate: pubdate,
-      tags: tags,
-      origin_title: origin_title,
-      image: image,
-      binding: binding,
-      translator: translator,
-      catalog: catalog,
-      pages: pages,
-      images: images,
-      alt: alt,
-      id: id,
-      publisher: publisher,
-      isbn10: isbn10, 
-      isbn13: isbn13,
-      title: title,
-      url: url,
-      alt_title: alt_title,
-      author_intro: author_intro,
-      summary: summary,
-      price: price,
-      json: json
-    }
+  defp process_url(book_id) do
+    book_id = if is_integer(book_id) do 
+      Integer.to_string(book_id) 
+    else
+      book_id
+    end
+    "https://api.douban.com/v2/book/" <> book_id
   end
 end
