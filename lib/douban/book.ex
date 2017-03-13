8d113e22d267813eea6c723d2a08fad36ad3c395
lyhopq <lyhopq@163.com>
2017-03-13 22:41:17 +0800

    get book by isbn

--- lib/douban/book.ex
+++ lib/douban/book.ex
@@ -4,7 +4,15 @@ defmodule Douban.Book do
   alias Douban.{Client, Book}
 
   def get_book_by_id(%Client{} = client, book_id) do
-    OAuth2.Client.get!(client.oauth, "/book/#{book_id}")
+    get_book(client, "/book/#{book_id}")
+  end
+
+  def get_book_by_isbn(%Client{} = client, isbn) do
+    get_book(client, "/book/isbn/#{isbn}")
+  end
+
+  defp get_book(%Client{} = client, url) do
+    OAuth2.Client.get!(client.oauth, url)
     |> Map.fetch!(:body)
     |> Enum.reduce(%{}, fn(x, acc) ->
       {k, v} = x;
